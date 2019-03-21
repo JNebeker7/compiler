3647426dbb766d2fe11d2b3410620327cc6ad693
Jared Nebeker <JNebeke4@gmail.com>
2019-03-21 14:20:47 -0600

    going to test no output

--- Parser.cpp
+++ Parser.cpp
@@ -31,33 +31,140 @@ void ParserClass::StatementGroup() {
 bool ParserClass::Statement() {
     TokenClass tc = mScanner->PeekNextToken();
     if (tc.GetTokenType() == INT_TOKEN) {
-        // int token
-        IntegerStatement();
-        // semi colon token
+        DeclarationStatement();
     } else if (tc.GetTokenType() == IDENTIFIER_TOKEN) {
-        IdentifierStatement();
-        // assignment token
-        // Expression()
-        // Semi colon token
+        AssignmentStatement();
     } else if (tc.GetTokenType() == COUT_TOKEN) {
-        // cout token
-        // insertion token
         CoutStatement();
-        // semi colon token
     }
     return false;
 }
 
-bool ParserClass::IntegerStatement() {
+bool ParserClass::DeclarationStatement() {
+    Match(INT_TOKEN);
+    Identifier();
+    Match(SEMICOLON_TOKEN);
     return true;
 }
 
-bool ParserClass::IdentifierStatement() {
+bool ParserClass::AssignmentStatement() {
+    Identifier();
+    Match(ASSIGNMENT_TOKEN);
+    Expression();
+    Match(SEMICOLON_TOKEN);
     return true;
 }
 
 bool ParserClass::CoutStatement() {
     return true;
+}
+
+void ParserClass::Identifier() {
+    TokenClass tt = Match(IDENTIFIER_TOKEN);
+    // check if already exists after you match tt
+    mSymbolTable->AddEntry(tt.GetLexeme()); // lexeme is i
+}
+
+void ParserClass::Integer() {
+    Match(INT_TOKEN);
+}
+
+void ParserClass::Expression() {
+    Relational();
+}
+
+void ParserClass::Relational() {
+    PlusMinus();
+    // Handle the optional tail:
+    TokenType tt = mScanner->PeekNextToken().GetTokenType();
+    switch (tt)
+    {
+        case LESS_TOKEN:
+            Match(tt);
+            PlusMinus();
+            break;
+        case LESSEQUAL_TOKEN:
+            Match(tt);
+            PlusMinus();
+            break;
+        case GREATER_TOKEN:
+            Match(tt);
+            PlusMinus();
+            break;
+        case GREATEREQUAL_TOKEN:
+            Match(tt);
+            PlusMinus();
+            break;
+        case EQUAL_TOKEN:
+            Match(tt);
+            PlusMinus();
+            break;
+        case NOTEQUAL_TOKEN:
+            Match(tt);
+            PlusMinus();
+            break;
+        default:
+            return;
+    }
+}
+
+void ParserClass::PlusMinus() {
+    TimesDivide();
+    while ( true ) {
+        TokenType tt = mScanner->PeekNextToken().GetTokenType();
+        switch (tt)
+        {
+            case PLUS_TOKEN:
+                Match(tt);
+                TimesDivide();
+                break;
+            case MINUS_TOKEN:
+                Match(tt);
+                TimesDivide();
+                break;
+            default:
+                return;
+        }
+    }
+}
+
+void ParserClass::TimesDivide() {
+    Factor();
+    while ( true ) {
+        TokenType tt = mScanner->PeekNextToken().GetTokenType();
+        switch (tt)
+        {
+            case TIMES_TOKEN:
+                Match(tt);
+                break;
+            case DIVIDE_TOKEN:
+                Match(tt);
+                break;
+            default:
+                return;
+        }
+    }
+}
+
+void ParserClass::Factor() {
+    TokenType tt = mScanner->PeekNextToken().GetTokenType();
+    switch (tt)
+    {
+        case IDENTIFIER_TOKEN:
+            Identifier();
+            break;
+        case INTEGER_TOKEN:
+            Integer();
+        case LPAREN_TOKEN:
+            Match(LPAREN_TOKEN);
+            Expression();
+            Match(RPAREN_TOKEN);
+            break;
+        default:
+            cerr << "There was an error in the Factor method" << endl;
+            exit(1);
+            break;
+    }
 }
 
 TokenClass ParserClass::Match(TokenType expectedType) {
--- include/Parser.h
+++ include/Parser.h
@@ -5,23 +5,32 @@
 #include "Debug.h"
 #include "Token.h"
 #include "Scanner.h"
+#include "Symbol.h"
 #include <iostream>
 
 class ParserClass {
 public:
-    ParserClass(ScannerClass *scanner)
-        : mScanner(scanner) {};
+    ParserClass(ScannerClass *scanner, SymbolTable *symbolTable)
+        : mScanner(scanner), mSymbolTable(symbolTable) {};
     void Start();
     void Program();
     void Block();
     void StatementGroup();
     bool Statement();
-    bool IntegerStatement();
-    bool IdentifierStatement();
+    bool DeclarationStatement();
+    bool AssignmentStatement();
     bool CoutStatement();
+    void Identifier();
+    void Expression();
+    void Relational();
+    void PlusMinus();
+    void TimesDivide();
+    void Factor();
+    void Integer();
     TokenClass Match(TokenType expectedType);
 private:
     ScannerClass *mScanner;
+    SymbolTable *mSymbolTable;
 };
 
 
